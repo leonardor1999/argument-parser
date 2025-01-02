1f6933a1676b22d4d54b0763d64ea8f6129f44fd
Leonardor99 <leonardo.russo@hotmail.it>
2025-01-02 11:02:55 +0100

    Add project files.

---
+++ ArgumentParser/ArgumentParser.cpp
@@ -0,0 +1,12 @@
+﻿// ArgumentParser.cpp : Defines the entry point for the application.
+//
+
+#include "ArgumentParser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
